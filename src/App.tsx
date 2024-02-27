33f7f395e8e45e6b424bc370e3b9067c5fa22630
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-02-27 18:43:46 +0100

    Create todos-context

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,16 @@
-import { useState } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
-import Todo from "./models/todo";
+
+import TodosContextProvider from "./store/todos-context";
 
 function App() {
-	const initItems = [new Todo("learn React"), new Todo("learn TypeScript")];
-
-	const [items, setItems] = useState<Todo[]>([]);
-
-	const addTodoHandler = (item: string) => {
-		// setItems([...items, new Todo(item)]);
-		setItems((prevItems) => [...prevItems, new Todo(item)]);
-	};
-
-	const deleteTodoHandler = (id: string) => {
-		setItems((prevItems) => {
-			const newItems = prevItems.filter((item) => item.id !== id);
-
-			return newItems;
-		});
-	};
+	
 	return (
-		<div>
-			<NewTodo onAddTodo={addTodoHandler} />
-			<Todos onDelete={deleteTodoHandler} items={items} />
-		</div>
+		<TodosContextProvider>
+			<NewTodo />
+			<Todos />
+		</TodosContextProvider>
 	);
 }
 
